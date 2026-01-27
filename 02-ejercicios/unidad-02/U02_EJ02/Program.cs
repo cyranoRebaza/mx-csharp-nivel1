5d784450fd52b0fd28a2b9255eb5fbe47b7f82e4
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-01-27 15:32:56 -0300

    Unidad 02 - Resolución ejercicio 2

---
+++ 02-ejercicios/unidad-02/U02_EJ02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace U02_EJ02
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            2. Hacer un programa para solicitar por teclado un número y luego devolver su valor elevado al cubo.
+            
+             */
+
+            // Declarar las variables
+            int numero;
+            int numeroCubo;
+
+            // Pedir datos
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            // Calcular
+            numeroCubo = numero * numero * numero;
+
+            // Mostrar
+            Console.WriteLine($"El numero {numero} elevado al cubo es: {numeroCubo}");
+
+            Console.ReadKey();
+        }
+    }
+
+}
