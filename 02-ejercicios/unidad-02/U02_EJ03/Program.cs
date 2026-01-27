8daa40cb74c4484341fc4d60400c46ad1edbca52
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-01-27 16:52:04 -0300

    Unidad 02 - Resolución ejercicio 3

---
+++ 02-ejercicios/unidad-02/U02_EJ03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace U02_EJ03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            3. Hacer un programa que permita ingresar el año actual y el año de la fecha de nacimiento 
+            de una persona y luego calcule y emita por pantalla su edad.
+
+            Nota: no hay que tener en cuenta si la persona cumplió años o no, simplemente calcular.
+
+             */
+
+
+        }
+    }
+
+}
