5c59331fdc54d80d925ac7a56348463b396cbb5a
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-01-28 22:59:44 -0300

    Unidad 02: resolución ejercicio 8

---
+++ 02-ejercicios/unidad-02/U02_EJ08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace U02_EJ08
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            8. Una importante cadena de delivery cuenta con una promoción por tiempo limitado en la que otorga 
+            un 15% de descuento sobre el total del valor de la compra realizada. Hacer un programa para solicitar 
+            el monto total y el mismo calcule y emita por pantalla el total a cobrar con el descuento aplicado.
+            
+             */
+
+            // Declaracion varibles
+            decimal montoTotal;
+            decimal totalCobrar;
+            decimal descuento;
+
+            const decimal PORCENTAJE_DESCUENTO = 0.15m;
+
+            // Pedir datos
+            Console.Write("Ingrese el monto total: ");
+            montoTotal = decimal.Parse(Console.ReadLine());
+
+            // Calcular
+            descuento = montoTotal * PORCENTAJE_DESCUENTO;
+            totalCobrar = montoTotal - descuento;
+
+            // Mostrar
+            Console.WriteLine($"El total a cobrar es: $ {totalCobrar:0.00}");
+        }
+    }
+
+}
