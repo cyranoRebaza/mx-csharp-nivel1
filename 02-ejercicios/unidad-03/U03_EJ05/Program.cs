dad9bba877a84d925a3e6662820d97ffadb1d8ca
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-01 18:16:54 -0300

    Unidad 03: resolución ejercicio 5

---
+++ 02-ejercicios/unidad-03/U03_EJ05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace U03_EJ05
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+           5. Hacer un programa para ingresar un número y mostrar por pantalla un cartel
+            aclaratorio si el mismo es PAR o IMPAR.                                                                                                                                                                                                                      aclaratorio si el mismo es PAR o IMPAR.
+                        
+             */
+
+            // Declaracion variables
+            int numero;
+
+            // Pedir datos
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            // Calcular y mostrar
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Es par");
+            }
+            else
+            {
+                Console.WriteLine("Es impar");
+            }
+        }
+    }
+
+}
