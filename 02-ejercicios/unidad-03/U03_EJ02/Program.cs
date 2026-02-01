0813414c7f403fbc35206e40c8500d95a7044b60
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-01 08:27:12 -0300

    Unidad 03: resolución ejercicio 2

---
+++ 02-ejercicios/unidad-03/U03_EJ02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace U03_EJ02
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            2. 2. Hacer un programa para ingresar dos números distintos y luego se muestre por
+   pantalla el menor de ellos.
+            
+             */
+
+            // Declaracion variables
+            int numero1;
+            int numero2;
+            int menor;
+
+            // Pedir datos
+            Console.WriteLine("--- Ingrese numeros distintos ---");
+            Console.Write("Ingrese un numero: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            // Calcular
+            if (numero1 < numero2)
+            {
+                menor = numero1;
+            }
+            else
+            {
+                menor = numero2;
+            }
+
+            // Mostrar
+            Console.WriteLine($"El numero menor es: {menor}");
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
