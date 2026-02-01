9860f67eab13d65aa734b5fb8dd757e9b4f5e526
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-01 08:06:52 -0300

    Unidad 03: resolución ejercicio 1

---
+++ 02-ejercicios/unidad-03/U03_EJ01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace U03_EJ01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            1. Hacer un programa para ingresar un número y luego se emita por pantalla un
+            cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+
+            */
+            // Declaracion variables
+            int numero;
+
+            // Pedir datos
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            // Preguntar y mostrar
+            if (numero > 10)
+            {
+                Console.WriteLine("Es mayor a 10");
+            }
+            else
+            {
+                Console.WriteLine("No es mayor a 10");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
