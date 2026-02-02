88344b8395eee6c3b298a13fe335b251d0d75b99
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-02 14:11:51 -0300

    Unidad 03: resolución ejercicio 10

---
+++ 02-ejercicios/unidad-03/U03_EJ10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace U03_EJ10
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            10. Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+            cuáles son mayores a 100.
+            
+             */
+
+            //  Declaracion variables
+            int numero1;
+            int numero2;
+            int numero3;
+            int numero4;
+
+            const int limiteNumero = 100;
+
+            // Pedir datos
+            Console.Write("Ingrese un numero: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero4 = int.Parse(Console.ReadLine());
+
+            // Calcular y mostrar
+            if (numero1 > limiteNumero)
+            {
+                Console.WriteLine(numero1);
+            }
+            if (numero2 > limiteNumero)
+            {
+                Console.WriteLine(numero2);
+            }
+            if (numero3 > limiteNumero)
+            {
+                Console.WriteLine(numero3);
+            }
+            if (numero4 > limiteNumero)
+            {
+                Console.WriteLine(numero4);
+            }
+        }
+    }
+
+}
