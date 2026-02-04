c093f58c5fdab949da262e076433c6b81186138a
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-04 18:33:51 -0300

    Unidad 04: operadores logicos and, or not

---
+++ 01-teoria/unidad-04/01-operadoresLogicos/U04_T01_operadoresLogicos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace U04_T01_operadoresLogicos
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND (&&) : ambos verdadero
+
+            //    true   &&  true  = true
+            //    true   &&  false = false
+            //    false  &&  true  = false
+            //    false  &&  false = false
+
+            int edad = 20;
+            char inicial = 'J';
+
+            if (edad > 20 && inicial == 'H')
+            {
+                Console.WriteLine("Tedras un descuento de 20 %");
+            }
+
+
+            // OR (||): con un solo verdadero es sufiente los demas es falso 
+
+            //    true   &&  true  = true
+            //    true   &&  false = true
+            //    false  &&  true  = true
+            //    false  &&  false = false
+
+            if (edad > 20 && (inicial == 'H' || inicial == 'J'))
+            {
+                Console.WriteLine("Tedras un descuento de 20 %");
+            }
+
+            // NOT (!) : niego una condicion
+            // dar un descuento a todos que sea mayores a 20, pero que no tengan iniciales a "I" o "J         
+
+            if (edad > 20 && !(inicial == 'H' || inicial == 'J'))
+            {
+                Console.WriteLine("Tedras un descuento de 20 %");
+            }
+        }
+    }
+
+}
