80a6fe3430889f79647da1190bcd707023b17a6e
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-04 20:45:04 -0300

    Unidad 04: sintaxis del switch

---
+++ 01-teoria/unidad-04/02-switch/U04_T02_switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace U04_T02_switch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // switch
+            // en vez de una condicion logica, tiene es una variables a evaluar
+            // Evalua secuencialmente cuando encuentra una coincidencia ejecuta y termina
+
+            char letra = 'A';
+
+            switch (letra)
+            {
+                case 'M':
+                    // Codigo
+                    break;
+                case 'H':
+                    // codigo
+                    break;
+                default:
+                    // no coincide con ninguna
+                    break;
+            }
+
+        }
+    }
+
+}
