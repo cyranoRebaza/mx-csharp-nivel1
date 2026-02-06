d16bfd620660c14ea5573e7538a8370a657c7c70
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-06 13:50:21 -0300

    Unidad 04: resolución ejercicio 12

---
+++ 02-ejercicios/unidad-04/U04_EJ12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace U04_EJ12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            12. Hacer un programa para ingresar tres números y emitir un cartel aclaratorio si
+            la suma de los dos primeros es mayor al producto del segundo con el tercero.
+            
+             */
+
+            //  Declaracion variables
+            int numero1;
+            int numero2;
+            int numero3;
+
+            int suma = 0;
+            int producto = 0;
+
+            // Pedir datos
+            Console.Write("Ingrese un numero: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un numero: ");
+            numero3 = int.Parse(Console.ReadLine());
+
+            // Calcular
+            suma = numero1 + numero2;
+            producto = numero2 * numero3;
+
+            if (suma > producto)
+            {
+                Console.WriteLine("La suma es mayor que el producto");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
