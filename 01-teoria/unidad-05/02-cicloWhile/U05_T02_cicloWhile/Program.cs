cc048bd41cc0320d331acde40fdfcc6ef620dbb5
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-08 09:07:02 -0300

    Unidad 05: ejemplos de la estructura while

---
+++ 01-teoria/unidad-05/02-cicloWhile/U05_T02_cicloWhile/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace U05_T02_cicloWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            CICLO WHILE
+
+            SINTAXIS:
+           
+             while(condicion){
+                 
+                 // codigo                
+
+             }
+
+             */
+
+            Console.WriteLine("--- EJEMPLO 1 ---");
+
+            // EJEMPLO 1: cargar una cantidad indeterminda de numero mientras sea numero sea distinto de cero
+            // luego mostrar cuantos fueron
+            int numero;
+            int contador = 0;
+
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero != 0)
+            {
+                contador++;
+
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine($"La cantidad de numeros ingresados es: {contador}");
+
+            Console.WriteLine("--- EJEMPLO 2 ---");
+
+            // EJEMPLO2: mostrar numeros del 1 al 10
+            int numero2;
+            int cantidadNumeros = 0;
+
+            while (cantidadNumeros < 10)
+            {
+                cantidadNumeros++;
+                Console.WriteLine(cantidadNumeros);
+            }
+
+
+
+        }
+    }
+
+}
