cdce123fbed99504d7bd25ee08c5d7f7d3bb72c8
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-09 12:45:01 -0300

    Unidad 05: ejemplos de la estructura do while

---
+++ 01-teoria/unidad-05/03-cicloDoWhile/U05_T03_cicloDoWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace U05_T03_cicloDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CICLO DO WHILE
+            // EJEMPLO 1: Mostrrar la cantidad de numeros ingresados,cortar con cero
+            int numero;
+            int contador = 0;
+
+            do
+            {
+
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero != 0)
+                {
+                    contador++;
+                }
+
+
+            } while (numero != 0);
+
+            Console.WriteLine($"Ingresaste {contador} numeros");
+        }
+    }
+
+}
