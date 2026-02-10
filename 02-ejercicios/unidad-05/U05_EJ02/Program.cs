27dd0f99d9fedaf1b2e4b5db2e458a7dbb278798
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-10 00:22:04 -0300

    Unidad 05: resolución ejercicio 02

---
+++ 02-ejercicios/unidad-05/U05_EJ02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace U05_EJ02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            2. Hacer un programa que solicite 20 números y calcule y emita por pantalla
+            cuántos son positivos (mayores a cero). Se debe mostrar un solo valor: el
+            conteo final.
+            
+             */
+
+            // Declaracion variables
+            int numero;
+            int contadorPositivos = 0;
+
+            // pedir datos
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > 0)
+                {
+                    contadorPositivos++;
+                }
+            }
+
+            // mostrar
+            Console.WriteLine($"La cantidad de numeros positivos es {contadorPositivos}");
+
+            Console.ReadKey();
+        }
+    }
+
+}
