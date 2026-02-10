9c388d3959b97478b0f05416a4d7e5a98e871851
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-10 01:49:33 -0300

    Unidad 05: resolución ejercicio 04

---
+++ 02-ejercicios/unidad-05/U05_EJ04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace U05_EJ04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            4. Hacer un programa para mostrar los números del 10 al 1. No se debe realizar
+            ningún pedido de datos.
+            
+             */
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
