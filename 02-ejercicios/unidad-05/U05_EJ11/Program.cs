6bfecfcb5d522bb1e600d49462ab8d42312c0c22
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-12 14:12:51 -0300

    Unidad 05: resolución ejercicio 11

---
+++ 02-ejercicios/unidad-05/U05_EJ11/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace U05_EJ11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            11. Hacer un programa para ingresar 10 números y luego calcule y emita el mayor
+            de los primos de la lista. En caso de no haber ningún número primo, deberá
+            aclararlo con un cartel.
+            
+             */
+
+            // Declaracion variables
+            int numero;
+
+            int mayorPrimo = 0;
+            bool hayPrimo = false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                // buscar primo
+                bool esPrimo = true;
+
+                if (numero <= 1)
+                {
+                    esPrimo = false;
+                }
+                else
+                {
+                    // busco divisores desde 2 hasta la raiz del numero
+                    for (int divisor = 2; divisor * divisor <= numero; divisor++)
+                    {
+                        if (numero % divisor == 0)
+                        {
+                            esPrimo = false;
+                            break;
+                        }
+                    }
+                }
+
+                // buscar el mayor de los primos
+                if (esPrimo)
+                {
+                    if (!hayPrimo || numero > mayorPrimo)
+                    {
+                        mayorPrimo = numero;
+                        hayPrimo = true;
+                    }
+                }
+
+
+            }
+
+            // Mostrar resultado
+            if (hayPrimo)
+            {
+                Console.WriteLine($"El mayor de los numeros primos es: {mayorPrimo}");
+
+            }
+            else
+            {
+                Console.WriteLine("No hay primos");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
