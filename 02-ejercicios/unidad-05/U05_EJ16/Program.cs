9813b11b886df5097e0dbab0b647270e4a4adf67
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-12 16:23:51 -0300

    Unidad 05: resolución ejercicio 16

---
+++ 02-ejercicios/unidad-05/U05_EJ16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace U05_EJ16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            16. Hacer un programa que muestre los números del 1 al 100 de 5 en 5. Ejemplo:
+            0, 5, 10, 15, 20…. 100. Usando While.
+            
+             */
+
+            // Declaracion variables
+            int iterador = 0;
+
+            const int ITERADOR_LIMITE = 100;
+
+            while (iterador <= ITERADOR_LIMITE)
+            {
+                Console.WriteLine(iterador);
+                iterador += 5;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
