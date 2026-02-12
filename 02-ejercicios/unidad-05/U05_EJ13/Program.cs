9ed9ba823db967f3f2d64e5ebf6a7aafa1444dae
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-12 14:39:22 -0300

    Unidad 05: resolución ejercicio 13

---
+++ 02-ejercicios/unidad-05/U05_EJ13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace U05_EJ13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            13. Hacer un programa para mostrar los números del 10 al 1. No se debe realizar
+            ningún pedido de datos. USANDO WHILE.
+            
+             */
+
+            // Declaracion de variables
+            int iterador = 10;
+
+            while (iterador >= 1)
+            {
+                Console.WriteLine(iterador);
+                iterador--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
