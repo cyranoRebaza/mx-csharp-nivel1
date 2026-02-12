ef649a80f1db439ab304879ec0e777d3494cd89b
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-11 22:16:29 -0300

    Unidad 05: resolución ejercicio 07

---
+++ 02-ejercicios/unidad-05/U05_EJ07/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace U05_EJ07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            7. Hacer un programa que solicite 10 números y luego mostrar por pantalla el
+            máximo de ellos y la posición en la que fue ingresado.
+            
+             */
+
+            // Declaracion variables
+            int numero;
+            int maximo = 0;
+            int posicionMaximo = 0;
+
+            // Pedir datos y buscar el mayor
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (i == 0 || numero > maximo)
+                {
+                    maximo = numero;
+                    posicionMaximo = i + 1;
+                }
+
+            }
+
+            // Mostrar numero mayor y su posicion
+            Console.WriteLine($"numero mayor es: {maximo}");
+            Console.WriteLine($"posicion numero mayor es: {posicionMaximo}");
+
+            Console.ReadKey();
+        }
+    }
+
+}
