7babcc5c5a357adcdaf2ad234fa0ab5698a98533
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-11 21:11:05 -0300

    Unidad 05: resolución ejercicio 05

---
+++ 02-ejercicios/unidad-05/U05_EJ05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace U05_EJ05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            5. Hacer un programa que muestre los números del 1 al 100 de 5 en 5. Ejemplo:
+            0, 5, 10, 15, 20…. 100.
+            
+             */
+
+            // Mostrar los numeros de 0 a 100 de 5 en 5
+            for (int i = 0; i <= 100; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
