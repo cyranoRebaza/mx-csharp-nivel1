618c2b0e8d97c70169ff11be69a2d5eca80c20e4
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-13 12:49:59 -0300

    Unidad 05: resolución ejercicio 19

---
+++ 02-ejercicios/unidad-05/U05_EJ19/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace U05_EJ19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            19. Hacer un programa que solicite una lista de números que corta cuando se
+            ingresa un cero y luego mostrar por pantalla el menor y el segundo menor.
+            
+             */
+
+            // Declaracion variables
+            int numero;
+            int primerMenor = 0;
+            int segundoMenor = 0;
+
+            bool hayPrimerMenor = false;
+            bool haySegundoMenor = false;
+
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero != 0)
+            {
+                if (!hayPrimerMenor)
+                {
+                    primerMenor = numero;
+                    hayPrimerMenor = true;
+                }
+                else
+                {
+                    if (!haySegundoMenor)
+                    {
+                        haySegundoMenor = true;
+
+                        if (numero < primerMenor)
+                        {
+                            segundoMenor = primerMenor;
+                            primerMenor = numero;
+                        }
+                        else
+                        {
+                            segundoMenor = numero;
+                        }
+                    }
+                    else
+                    {
+                        if (numero < primerMenor)
+                        {
+                            segundoMenor = primerMenor;
+                            primerMenor = numero;
+                        }
+                        else
+                        {
+                            if (numero < segundoMenor)
+                            {
+                                segundoMenor = numero;
+                            }
+                        }
+                    }
+                }
+
+
+
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            // Mostrar resultados
+            if (hayPrimerMenor && haySegundoMenor)
+            {
+
+                Console.WriteLine($"El primer menor es: {primerMenor}");
+                Console.WriteLine($"El segundo menor es: {segundoMenor}");
+            }
+            else
+            {
+                Console.WriteLine("se ingreso un solo numero");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
