1c8928ee0f450376d9896bb22d935dde8d9f65d6
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-02-26 13:00:39 -0300

    Unidad 06: resolución ejercicio 06

---
+++ 02-ejercicios/unidad-06/U06_EJ06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace U06_EJ06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            6. Hacer un programa para ingresar 10 números. El mismo debe analizar y mostrar por pantalla cuántos de esos números son primos.
+            
+             */
+
+            // Declaracion variables
+            int numero;
+            int cantidadDivisores;
+            int cantidadPrimos = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > 1)
+                {
+                    cantidadDivisores = 0;
+
+                    for (int j = 1; j <= numero; j++)
+                    {
+                        if (numero % j == 0)
+                        {
+                            cantidadDivisores++;
+                        }
+                    }
+
+                    if (cantidadDivisores == 2)
+                    {
+                        cantidadPrimos++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"La cantidad de numeros primos es: {cantidadPrimos}");
+        }
+    }
+
+}
