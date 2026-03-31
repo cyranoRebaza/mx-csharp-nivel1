b7d3fba15f4d5a59cc18d63f018c879fbf029706
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-03-31 16:31:44 -0300

    feat: unidad 07 - resolución ejercicio 01

---
+++ 02-ejercicios/unidad-07/U07_EJ01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace U07_EJ01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 
+            1. Hacer un programa que solicite 50 números enteros y los guarde en un vector.
+            Luego recorrer el vector y determinar e informar cuál es la suma de los valores
+            del mismo.
+            Nota: usar dos ciclos: uno para guardar los números en el vector y otro para
+            recorrerlo y leerlo.
+             */
+
+            // Declaraciones
+            int numero;
+            int[] numeros = new int[50];
+
+            // Cargar el vector
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine()!);
+
+                numeros[i] = numero;
+
+            }
+
+            // leer y acumular
+            int sumaValores = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                sumaValores += numeros[i];
+            }
+
+            // mostrar
+            Console.WriteLine($"La suma de los valores es: {sumaValores}");
+        }
+    }
+}
