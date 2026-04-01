0c571dab8fe39197a7494cbde1123c884cdc638d
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-04-01 11:17:34 -0300

    feat: unidad 07 - resolución ejercicio 03

---
+++ 02-ejercicios/unidad-07/U07_EJ03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace U07_EJ03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            3. Hacer un programa que solicite 100 números enteros y los guarde en un
+            vector. Luego recorrer ese vector para calcular el promedio. Mostrar por
+            pantalla los valores del vector que son mayores al promedio calculado
+
+             */
+
+            // Declaracion de variables
+            int numero;
+            int[] numeros = new int[100];
+
+
+            // Cargar el vector
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Ingrese un numero:");
+                numero = int.Parse(Console.ReadLine()!);
+
+                numeros[i] = numero;
+            }
+
+            // leer el vector y calcular el promedio
+            int acumular = 0;
+            int contador = 0;
+            float promedio = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                acumular += numeros[i];
+                contador++;
+            }
+
+            promedio = (float)acumular / contador;
+
+            // buscar y mostrar el mayor
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > promedio)
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
+
+
+        }
+    }
+}
